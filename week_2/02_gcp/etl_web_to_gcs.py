3e72460bf6fdd42c39c3d0b17ba62ab2ba791426
“Dmitri <dima.piminov@gmail.com>
2023-02-08 19:57:24 +0800

    add etl_gcs_to_bq minor change in etl_web_to_gcs

--- week_2/02_gcp/etl_web_to_gcs.py
+++ week_2/02_gcp/etl_web_to_gcs.py
@@ -48,10 +48,10 @@ def write_gcs(path: Path) -> None:
 
 @flow()
 def etl_web_to_gcs() -> None:
-    """The main ETL function"""
-    color = "green"
-    year = 2020
-    month = 1
+    """The main ETL function""" 
+    color = "yellow"
+    year = 2019
+    month = 3
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
